068d8f9dc82b5332f1df806e28c011fc9fedb9f2
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2007-01-26 07:54:00 +0000

    now uses commands
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@99 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestPlugin/src/main/java/org/cubictest/ui/gef/actions/CutAction.java
+++ CubicTestPlugin/src/main/java/org/cubictest/ui/gef/actions/CutAction.java
@@ -8,8 +8,14 @@ package org.cubictest.ui.gef.actions;
 import java.util.Iterator;
 import java.util.List;
 
+import org.cubictest.model.AbstractPage;
 import org.cubictest.model.PageElement;
+import org.cubictest.model.Test;
 import org.cubictest.model.context.IContext;
+import org.cubictest.ui.gef.command.CreatePageElementCommand;
+import org.cubictest.ui.gef.command.DeletePageCommand;
+import org.cubictest.ui.gef.command.DeletePageElementCommand;
+import org.cubictest.ui.utils.ViewUtil;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.ui.actions.Clipboard;
 import org.eclipse.gef.ui.actions.SelectionAction;
@@ -59,8 +65,16 @@ public class CutAction extends SelectionAction{
 		while(iter.hasNext()) {
 			EditPart item = (EditPart) iter.next();
 			if (item.getModel() instanceof PageElement) {
-				IContext container = (IContext) item.getParent().getModel();
-				container.removeElement((PageElement) item.getModel());
+				DeletePageElementCommand deleteCmd = new DeletePageElementCommand();
+				deleteCmd.setContext((IContext) item.getParent().getModel());
+				deleteCmd.setPageElement((PageElement) item.getModel());
+				getCommandStack().execute(deleteCmd);
+			}
+			else if (item.getModel() instanceof AbstractPage) {
+				DeletePageCommand deleteCmd = new DeletePageCommand();
+				deleteCmd.setTest((Test) item.getParent().getModel());
+				deleteCmd.setTransitionNode((AbstractPage) item.getModel());
+				getCommandStack().execute(deleteCmd);
 			}
 		}
 	}
