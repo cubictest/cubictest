34077fb60a998e7cd548b982524068c124b74a11
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2007-02-16 21:36:26 +0000

    better javadoc
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@148 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestWatirExporter/src/org/cubictest/exporters/watir/converters/UrlStartPointConverter.java
+++ CubicTestWatirExporter/src/org/cubictest/exporters/watir/converters/UrlStartPointConverter.java
@@ -14,17 +14,16 @@ import org.cubictest.exporters.watir.holders.TestStep;
 import org.cubictest.model.UrlStartPoint;
 
 /**
- * Class for converting Connection points to Watir steps.
+ * Class for converting UrlStartPoint to Watir steps.
  * 
  * @author chr_schwarz
  */
 public class UrlStartPointConverter implements IUrlStartPointConverter<IStepList> {
 	
 	
-	public void handleUrlStartPoint(IStepList steps, UrlStartPoint cp) {
-		UrlStartPoint sp = (UrlStartPoint) cp;
+	public void handleUrlStartPoint(IStepList steps, UrlStartPoint sp) {
 		String start = "ie.goto(\"" + sp.getBeginAt() + "\")";
-		ITestStep step = new TestStep(start).setDescription("Invoking page: " + sp.getBeginAt());
+		ITestStep step = new TestStep(start).setDescription("Opening " + sp);
 		steps.add(step);
 	}
 }
