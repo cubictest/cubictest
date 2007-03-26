8173f7a3f81e49da97f2e2a75c60ba855f1560c0
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2007-03-26 08:27:13 +0000

    fixed adding of elements in context
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@310 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestPlugin/src/main/java/org/cubictest/ui/utils/UserInteractionDialogUtil.java
+++ CubicTestPlugin/src/main/java/org/cubictest/ui/utils/UserInteractionDialogUtil.java
@@ -62,7 +62,7 @@ public class UserInteractionDialogUtil {
 			}
 
 			if(element instanceof IContext){
-				getFlattenedPageElements(((IContext) element).getElements());
+				flattenedElements.addAll(getFlattenedPageElements(((IContext) element).getElements()));
 				flattenedElements.add(element);
 			}
 			else {
