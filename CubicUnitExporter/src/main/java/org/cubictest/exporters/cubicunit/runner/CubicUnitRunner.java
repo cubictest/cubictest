c94a89dc2463d8d6eca95a78c77247f706bdaac1
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2007-04-02 20:58:28 +0000

    refactored out of the main plugin
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@381 547eb39c-8521-0410-b6bb-ad8a2341d8b0

---
+++ CubicUnitExporter/src/main/java/org/cubictest/exporters/cubicunit/runner/CubicUnitRunner.java
@@ -0,0 +1,51 @@
+/*
+ * Created on 04.aug.2006
+ * 
+ * This software is licensed under the terms of the GNU GENERAL PUBLIC LICENSE
+ * Version 2, which can be found at http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+package org.cubictest.exporters.cubicunit.runner;
+
+import org.cubictest.export.converters.TreeTestWalker;
+import org.cubictest.exporters.cubicunit.runner.converters.ContextConverter;
+import org.cubictest.exporters.cubicunit.runner.converters.CustomTestStepConverter;
+import org.cubictest.exporters.cubicunit.runner.converters.ElementConverter;
+import org.cubictest.exporters.cubicunit.runner.converters.Holder;
+import org.cubictest.exporters.cubicunit.runner.converters.TransitionConverter;
+import org.cubictest.exporters.cubicunit.runner.converters.UrlStartPointConverter;
+import org.cubictest.model.Test;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+
+public class CubicUnitRunner implements IRunnableWithProgress, ILaunchConfigurationDelegate{
+
+	private Test test;
+	
+	public CubicUnitRunner(Test test) {
+		this.test = test;
+	}
+	
+	public void run(IProgressMonitor monitor) {
+		//	Set up dependency hierarchy:
+		Holder holder = new Holder();
+		
+		TreeTestWalker<Holder> testWalker = new TreeTestWalker<Holder>(UrlStartPointConverter.class, 
+				ElementConverter.class, ContextConverter.class, 
+				TransitionConverter.class,CustomTestStepConverter.class);
+		
+		monitor.beginTask("Traversing the test model...", IProgressMonitor.UNKNOWN);
+		
+		testWalker.convertTest(test, holder);
+		
+		monitor.done();
+	}
+
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
+	}
+
+}
---
+++ CubicUnitExporter/src/main/java/org/cubictest/exporters/cubicunit/runner/converters/CustomTestStepConverter.java
@@ -0,0 +1,15 @@
+/*
+ * This software is licensed under the terms of the GNU GENERAL PUBLIC LICENSE
+ * Version 2, which can be found at http://www.gnu.org/copyleft/gpl.html
+ */
+package org.cubictest.exporters.cubicunit.runner.converters;
+
+import org.cubictest.export.converters.ICustomTestStepConverter;
+import org.cubictest.model.CustomTestStep;
+
+public class CustomTestStepConverter implements ICustomTestStepConverter<Holder> {
+
+	public void handleCustomStep(Holder holder, CustomTestStep cts) {
+		//cts.execute(holder.getContext(), holder.getDocument());
+	}
+}
