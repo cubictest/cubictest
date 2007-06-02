39f2eb5c81c62a438862544d4ec0f4c90bf76b32
skytteren <skytteren@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2007-06-02 15:53:18 +0000

    Adding custom step and trying to fix error
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@736 547eb39c-8521-0410-b6bb-ad8a2341d8b0

---
+++ CubicUnitExporter/src/main/java/org/cubictest/exporters/cubicunit/ui/CustomStepExtension.java
@@ -0,0 +1,118 @@
+package org.cubictest.exporters.cubicunit.ui;
+
+import org.cubictest.common.utils.ErrorHandler;
+import org.cubictest.exporters.cubicunit.ui.command.ChangeCubicUnitCustomStepCommand;
+import org.cubictest.model.customstep.data.CustomTestStepData;
+import org.cubictest.model.customstep.data.CustomTestStepDataEvent;
+import org.cubictest.model.customstep.data.ICustomTestStepDataListener;
+import org.cubictest.ui.customstep.section.CustomStepSection;
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.ui.IJavaElementSearchConstants;
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.SelectionDialog;
+
+public class CustomStepExtension extends CustomStepSection implements ICustomTestStepDataListener{
+
+	private Text classText;
+	private Link newClassLink;
+	private Button browserClassButton;
+	private CustomTestStepData data;
+	
+	@Override
+	public void createControl(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setBackground(ColorConstants.white);
+		
+		FormLayout formLayout = new FormLayout();
+		composite.setLayout(formLayout);
+		
+		newClassLink = new Link(composite, SWT.PUSH);
+		newClassLink.setText("<A>CubicUnit Selenium extension*: </A>");
+		newClassLink.setBackground(ColorConstants.white);
+		newClassLink.addSelectionListener(new SelectionListener(){
+			public void widgetDefaultSelected(SelectionEvent e) {}
+			public void widgetSelected(SelectionEvent e) {
+				CustomStepWizard customStepWizard = new CustomStepWizard();
+				WizardDialog dialog = new WizardDialog(new Shell(),customStepWizard);
+				if(dialog.open() == WizardDialog.OK){
+					String className = customStepWizard.getClassName();
+					classText.setText(className);
+				}
+			}
+		});
+		
+		FormData layoutData = new FormData();
+		layoutData.left = new FormAttachment(0,0);
+		layoutData.width = STANDARD_LABEL_WIDTH;
+		newClassLink.setLayoutData(layoutData);
+		
+		classText = new Text(composite,SWT.READ_ONLY|SWT.BORDER);
+		classText.setBackground(ColorConstants.white);
+		classText.setText(data.getDisplayText());
+		
+		layoutData = new FormData();
+		layoutData.left = new FormAttachment(newClassLink);
+		layoutData.width = STANDARD_LABEL_WIDTH * 2;
+		classText.setLayoutData(layoutData);
+		
+		browserClassButton = new Button(composite, SWT.PUSH);
+		browserClassButton.setText("Browse...");
+		browserClassButton.addSelectionListener(new SelectionListener(){
+			public void widgetDefaultSelected(SelectionEvent e) {}
+			public void widgetSelected(SelectionEvent e) {
+				Shell shell = new Shell();
+				SelectionDialog dialog;
+				try {
+					dialog = JavaUI.createTypeDialog(
+					        shell, new ProgressMonitorDialog(shell),
+					        SearchEngine.createWorkspaceScope(),
+					        IJavaElementSearchConstants.CONSIDER_CLASSES, false);
+					dialog.setTitle("Open ICustomTestStep implementation");
+			        if (dialog.open() == SelectionDialog.OK){
+			        	IType result = (IType) dialog.getResult()[0];
+			        	
+			        	ChangeCubicUnitCustomStepCommand command = 
+			        		new ChangeCubicUnitCustomStepCommand();
+			        	command.setCustomTestStepData(data);
+			        	command.setPath(result.getPath().toPortableString());
+			        	command.setDisplayText(result.getFullyQualifiedName());
+			        	getCommandStack().execute(command);
+			        }
+				} catch (JavaModelException ex) {
+					ErrorHandler.logAndShowErrorDialog(ex);
+				}
+		        
+			}
+		});
+		layoutData = new FormData();
+		layoutData.left = new FormAttachment(classText,5);
+		browserClassButton.setLayoutData(layoutData);
+	}
+	
+	@Override
+	public void setData(CustomTestStepData data) {
+		this.data = data;
+		data.addChangeListener(this);
+	}
+
+	public void handleEvent(CustomTestStepDataEvent event) {
+		classText.setText(data.getDisplayText());
+	}
+
+}
---
+++ CubicUnitExporter/src/main/java/org/cubictest/exporters/cubicunit/ui/CustomStepWizard.java
@@ -0,0 +1,49 @@
+package org.cubictest.exporters.cubicunit.ui;
+
+import org.cubictest.common.utils.ErrorHandler;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.ui.wizards.NewClassWizardPage;
+import org.eclipse.jface.wizard.Wizard;
+
+public class CustomStepWizard extends Wizard {
+
+	
+	private NewClassWizardPage classWizard;
+	private String name;
+	
+	public CustomStepWizard() {
+		setWindowTitle("Create new CustomTestStep");
+	}
+	
+	@Override
+	public void addPages() {
+		classWizard = new NewClassWizardPage();
+		classWizard.addSuperInterface("org.cubictest.custom.ICustomTestStep");
+		addPage(classWizard);
+	}
+	
+	@Override
+	public boolean performFinish() {
+		name = classWizard.getTypeName();
+		if(name != null && name.length() > 0){
+			try {
+				classWizard.createType(new NullProgressMonitor());
+				return true;
+			} catch (CoreException e) {
+				ErrorHandler.logAndShowErrorDialog(e);
+			} catch (InterruptedException e) {
+				ErrorHandler.logAndShowErrorDialog(e);
+			}
+		}
+		return false;
+	}
+	
+	public String getClassName(){
+		String packageText = classWizard.getPackageText();
+		String typeTypeName = classWizard.getTypeName();
+		return packageText + (packageText.length() > 0 ? "." : "") + typeTypeName;
+	}
+
+}
