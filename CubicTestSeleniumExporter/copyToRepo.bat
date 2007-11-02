1240b2cd3f633b64f773af934867df607327d2a1
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2007-11-02 20:42:22 +0000

    Fixed M2 runner support
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1071 547eb39c-8521-0410-b6bb-ad8a2341d8b0

---
+++ CubicTestSeleniumExporter/copyToRepo.bat
@@ -0,0 +1,2 @@
+copy "C:\projects\cubictest\trunk\CubicTestSeleniumExporter\selenium-exporter-1.7.11.jar" "D:\Documents and Settings\Christian Schwarz\.m2\repository\org\cubictest\selenium-exporter\1.7.11"
+pause
