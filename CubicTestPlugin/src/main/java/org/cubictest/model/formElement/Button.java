620d0eddbc07aba8b43e2c05989affa4b566290f
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2007-11-02 08:14:37 +0000

    now records image buttons correctly
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1067 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestPlugin/src/main/java/org/cubictest/model/formElement/Button.java
+++ CubicTestPlugin/src/main/java/org/cubictest/model/formElement/Button.java
@@ -6,8 +6,17 @@
 */
 package org.cubictest.model.formElement;
 
+import static org.cubictest.model.IdentifierType.CLASS;
+import static org.cubictest.model.IdentifierType.ID;
+import static org.cubictest.model.IdentifierType.LABEL;
+import static org.cubictest.model.IdentifierType.TITLE;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.cubictest.model.FormElement;
 import org.cubictest.model.Identifier;
+import org.cubictest.model.IdentifierType;
 
 
 /**
@@ -27,4 +36,12 @@ public class Button extends FormElement {
 	protected void setDefaultIdentifierValues() {
 		getIdentifiers().get(0).setProbability(Identifier.MAX_PROBABILITY);
 	}
+	
+	@Override
+	public List<IdentifierType> getIdentifierTypes() {	
+		List<IdentifierType> list = super.getIdentifierTypes();
+		list.add(IdentifierType.SRC);
+		return list;
+	}
+
 }
