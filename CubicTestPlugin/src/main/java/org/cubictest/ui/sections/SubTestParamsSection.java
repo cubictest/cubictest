143907956d7c1047ff04450bae226019b2886327
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2007-12-06 08:02:00 +0000

    added params for subtest
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1121 547eb39c-8521-0410-b6bb-ad8a2341d8b0

---
+++ CubicTestPlugin/src/main/java/org/cubictest/ui/sections/SubTestParamsSection.java
@@ -0,0 +1,145 @@
+/*
+ * Created on 17.may.2006
+ * 
+ * This software is licensed under the terms of the GNU GENERAL PUBLIC LICENSE
+ * Version 2, which can be found at http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+package org.cubictest.ui.sections;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.cubictest.model.SubTest;
+import org.cubictest.model.Test;
+import org.cubictest.model.parameterization.ParameterList;
+import org.cubictest.ui.gef.command.ChangeParameterListIndexCommand;
+import org.cubictest.ui.gef.controller.SubTestEditPart;
+import org.cubictest.ui.gef.editors.GraphicalTestEditor;
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.views.properties.tabbed.AbstractPropertySection;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
+
+/**
+ * Property section for parameterization of sub tests.
+ * 
+ * @author Christian Schwarz
+ */
+public class SubTestParamsSection extends AbstractPropertySection implements PropertyChangeListener {
+
+	private Test test;
+	private Label paramLabel;
+	private Spinner paramIndexSpinner;
+	
+	@Override
+	public void createControls(Composite parent, TabbedPropertySheetPage aTabbedPropertySheetPage) {
+		super.createControls(parent, aTabbedPropertySheetPage);
+		Composite composite = getWidgetFactory().createFlatFormComposite(parent);
+		
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 3;
+		gridLayout.makeColumnsEqualWidth = false;
+		
+		paramLabel = getWidgetFactory().createLabel(composite, "Parameter index:");
+		paramIndexSpinner = new Spinner(composite, SWT.BORDER);
+		paramIndexSpinner.addModifyListener(new ModifyListener(){
+			public void modifyText(ModifyEvent e) {
+				ChangeParameterListIndexCommand command = new ChangeParameterListIndexCommand();
+				command.setParameterList(test.getParamList());
+				command.setNewIndex(paramIndexSpinner.getSelection());
+				command.setTest(test);
+				executeCommand(command);
+			}
+		});
+		
+		composite.setLayout(gridLayout);
+	}
+	
+	private void updateIndexSpinner() {
+		if(test != null && null != test.getParamList()){
+			ParameterList list = test.getParamList();
+			int length = list.inputParameterSize();
+			paramIndexSpinner.setValues(list.getParameterIndex(), 0, (length <= 0) ? 0 : length - 1, 0, 1, 5);
+		}
+	}
+
+	@Override
+	public void setInput(IWorkbenchPart part, ISelection selection) {
+		super.setInput(part, selection);
+		if(test != null){
+			test.removePropertyChangeListener(this);
+			if(test.getParamList() != null)
+				test.getParamList().removePropertyChangeListener(this);
+		}
+		Assert.isTrue(selection instanceof IStructuredSelection);
+		Object input = ((IStructuredSelection) selection).getFirstElement();
+		Assert.isTrue(input instanceof SubTestEditPart);
+		test = ((SubTest) ((SubTestEditPart) input).getModel()).getTest(false);
+		test.addPropertyChangeListener(this);
+		if(test.getParamList() != null)
+			test.getParamList().addPropertyChangeListener(this);
+	}
+			
+	private void executeCommand(Command command) {
+		IWorkbenchPart part = getPart();
+		if(part instanceof GraphicalTestEditor)
+			((GraphicalTestEditor)part).getCommandStack().execute(command);
+		
+	}
+	
+	@Override
+	public void refresh() {
+		super.refresh();
+		boolean visibile = (test.getParamList() != null);
+		if (testHasParams()) {
+			paramLabel.setText("Parameter index");
+			paramLabel.redraw();
+		}
+		else {
+			paramLabel.setText("Test in subtest has no parameterization configured");
+			paramLabel.redraw();
+		}
+		paramIndexSpinner.setVisible(visibile && testHasParams());
+		
+		if(test.getParamList() != null) {
+			updateIndexSpinner();
+		}
+	}
+
+	private boolean testHasParams() {
+		if (test.getParamList() != null) {
+			return test.getParamList().inputParameterSize() > 0;
+		}
+		return false;
+	}
+
+	public void propertyChange(PropertyChangeEvent event) {
+		if(event.getOldValue() instanceof ParameterList){
+			((ParameterList)event.getOldValue()).removePropertyChangeListener(this);
+		}
+		if(event.getNewValue() instanceof ParameterList){
+			((ParameterList)event.getNewValue()).removePropertyChangeListener(this);
+		}
+		refresh();
+	}
+	
+	@Override
+	public void aboutToBeHidden() {
+		super.aboutToBeHidden();
+		test.removePropertyChangeListener(this);
+		if(test.getParamList() != null)
+			test.getParamList().removePropertyChangeListener(this);
+	}
+	
+}
