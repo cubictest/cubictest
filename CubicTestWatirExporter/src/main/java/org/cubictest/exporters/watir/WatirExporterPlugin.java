32c5f0f3367b5cd9714540ad7a8100e58d977cd9
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-01-07 20:08:14 +0000

    Changed to EPL license
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1209 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestWatirExporter/src/main/java/org/cubictest/exporters/watir/WatirExporterPlugin.java
+++ CubicTestWatirExporter/src/main/java/org/cubictest/exporters/watir/WatirExporterPlugin.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2008  Christian Schwarz
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Christian Schwarz - initial API and implementation
+ *******************************************************************************/
 package org.cubictest.exporters.watir;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
