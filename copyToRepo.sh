a5e609ecf233c6fa25406f004500d5b34c6dcfd6
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-01-31 10:54:46 +0000

    added checksums to m2 repo files
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1267 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- copyToRepo.sh
+++ copyToRepo.sh
@@ -1,11 +1,22 @@
 cd CubicTestPlugin
-cp cubictest*.jar ~/.m2/repository/org/cubictest/cubictest/1.8.1
-cp cubictest*.pom ~/.m2/repository/org/cubictest/cubictest/1.8.1
-cd ../CubicTestSeleniumExporter
-cp selenium-exporter*.jar ~/.m2/repository/org/cubictest/selenium-exporter/1.8.1
-cp selenium-exporter*.pom ~/.m2/repository/org/cubictest/selenium-exporter/1.8.1
-cd ../CubicTestMojo
-mvn install
-cd .. 
+cp cubictest-1.8.1.jar ~/.m2/repository/org/cubictest/cubictest/1.8.1
+sha1 cubictest-1.8.1.jar | awk '{print $1}' > ~/.m2/repository/org/cubictest/cubictest/1.8.1/cubictest-1.8.1.jar.sha1
+md5 cubictest-1.8.1.jar | awk '{print $1}' > ~/.m2/repository/org/cubictest/cubictest/1.8.1/cubictest-1.8.1.jar.md5
 
+cp cubictest-1.8.1.pom ~/.m2/repository/org/cubictest/cubictest/1.8.1
+sha1 cubictest-1.8.1.pom | awk '{print $1}' > ~/.m2/repository/org/cubictest/cubictest/1.8.1/cubictest-1.8.1.pom.sha1
+md5 cubictest-1.8.1.pom | awk '{print $1}' > ~/.m2/repository/org/cubictest/cubictest/1.8.1/cubictest-1.8.1.pom.md5
+
+cd ../CubicTestSeleniumExporter
+cp selenium-exporter-1.8.1.jar ~/.m2/repository/org/cubictest/selenium-exporter/1.8.1
+sha1 selenium-exporter-1.8.1.jar | awk '{print $1}' > ~/.m2/repository/org/cubictest/selenium-exporter/1.8.1/selenium-exporter-1.8.1.jar.sha1
+md5 selenium-exporter-1.8.1.jar | awk '{print $1}' > ~/.m2/repository/org/cubictest/selenium-exporter/1.8.1/selenium-exporter-1.8.1.jar.md5
+
+cp selenium-exporter-1.8.1.pom ~/.m2/repository/org/cubictest/selenium-exporter/1.8.1
+sha1 selenium-exporter-1.8.1.pom | awk '{print $1}' > ~/.m2/repository/org/cubictest/selenium-exporter/1.8.1/selenium-exporter-1.8.1.pom.sha1
+md5 selenium-exporter-1.8.1.pom | awk '{print $1}' > ~/.m2/repository/org/cubictest/selenium-exporter/1.8.1/selenium-exporter-1.8.1.pom.md5
+
+cd ../CubicTestMojo
+mvn clean install -DcreateChecksum=true
+cd ..
 
