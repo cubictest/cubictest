58c6773552beb5a3e22cb970486d40839b19eae7
skytteren <skytteren@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-02-05 22:25:08 +0000

    Actually getting the serverstuff to work.. but to bad it isn't ready yet...
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1307 547eb39c-8521-0410-b6bb-ad8a2341d8b0

---
+++ CubicTestSeleniumExporter/src/main/java/org/cubictest/exporters/selenium/launch/converters/CustomTestStepConverter.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2008 Christian Schwarz and Stein K. Skytteren
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Christian Schwarz and Stein K. Skytteren - initial API and implementation
+ *******************************************************************************/
+package org.cubictest.exporters.selenium.launch.converters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cubictest.export.converters.ICustomTestStepConverter;
+import org.cubictest.export.exceptions.ExporterException;
+import org.cubictest.exporters.selenium.runner.CubicTestRemoteRunnerClient;
+import org.cubictest.exporters.selenium.runner.holders.SeleniumHolder;
+import org.cubictest.model.ICustomTestStepHolder;
+import org.cubictest.model.TestPartStatus;
+import org.cubictest.model.customstep.CustomTestStepParameter;
+import org.cubictest.model.customstep.data.CustomTestStepData;
+
+/**
+ * Selenium custom test step converter.
+ * 
+ * @author chr_schwarz
+ */
+public class CustomTestStepConverter implements ICustomTestStepConverter<SeleniumHolder> {
+	
+	public String getDataKey() {
+		return "org.cubictest.seleniumexporter";
+	}
+
+	public void handleCustomStep(SeleniumHolder t, ICustomTestStepHolder cts,
+			CustomTestStepData data) {
+		//throw new ExporterException("Custom test step not supported in Selenium runner yet");
+		CubicTestRemoteRunnerClient runner = t.getCustomStepRunner();
+		
+		List<String> attributes = new ArrayList<String>();
+		attributes.add(data.getPath());
+		
+		for(CustomTestStepParameter param : cts.getCustomTestStepParameters()){
+			attributes.add(param.getKey());
+			attributes.add(cts.getValue(param).getValue());
+		}
+		
+		String result = runner.executeOnServer("customTestStep",
+				attributes.toArray(new String[attributes.size()]));
+		if(result.startsWith("ERROR")){
+			cts.setStatus(TestPartStatus.EXCEPTION);
+			throw new ExporterException(result.replaceFirst("Error: ", result));
+		}else 
+			cts.setStatus(TestPartStatus.PASS);
+	}
+
+}
---
+++ CubicTestSeleniumExporter/src/main/java/org/cubictest/exporters/selenium/ui/command/ChangeSeleniumCustomStepCommand.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2008  Stein K. Skytteren
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Stein K. Skytteren - initial API and implementation
+ *******************************************************************************/
+package org.cubictest.exporters.selenium.ui.command;
+
+import org.cubictest.model.customstep.data.CustomTestStepData;
+import org.eclipse.gef.commands.Command;
+
+public class ChangeSeleniumCustomStepCommand extends Command {
+
+	private CustomTestStepData customTestStepData;
+	private String oldDisplayText;
+	private String path;
+	private String displayText;
+	private String oldPath;
+
+	public void setCustomTestStepData(CustomTestStepData customTestStepData) {
+		this.customTestStepData = customTestStepData;
+	}
+
+	@Override
+	public void execute() {
+		super.execute();
+		oldDisplayText = customTestStepData.getDisplayText();
+		customTestStepData.setDisplayText(displayText);
+		oldPath = customTestStepData.getPath();
+		customTestStepData.setPath(path);
+	}
+	
+	@Override
+	public void undo() {
+		super.undo();
+		customTestStepData.setDisplayText(oldDisplayText);
+		customTestStepData.setPath(oldPath);
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public void setDisplayText(String displayText) {
+		this.displayText = displayText;
+	}
+}
