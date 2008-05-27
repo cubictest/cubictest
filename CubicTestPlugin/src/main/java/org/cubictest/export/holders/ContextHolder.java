b30690bb57bd3de9048a9039d3ccafc20e19ccd6
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-05-27 21:50:08 +0000

    CT-31: Adding support for frames in Watir exporter
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1491 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestPlugin/src/main/java/org/cubictest/export/holders/ContextHolder.java
+++ CubicTestPlugin/src/main/java/org/cubictest/export/holders/ContextHolder.java
@@ -208,6 +208,10 @@ public abstract class ContextHolder implements IResultHolder {
 		return getParentFrame(parent);
 	}
 	
+	public boolean isInAFrame(PageElement pe) {
+		return getParentFrame(pe) != null;
+	}
+	
 	public void setUseNamespace(boolean useNamespace) {
 		this.useNamespace = useNamespace;
 	}
