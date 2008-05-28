08978bcd133b8d3bb0dc5689c7af438299f29aed
skytteren <skytteren@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-05-28 19:53:11 +0000

    Adding source to the CubicTest classes.
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1500 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestSeleniumExporterServer/src/main/java/org/cubictest/selenium/custom/ICustomTestStep.java
+++ CubicTestSeleniumExporterServer/src/main/java/org/cubictest/selenium/custom/ICustomTestStep.java
@@ -4,10 +4,19 @@ import java.util.Map;
 
 import com.thoughtworks.selenium.Selenium;
 
+/**
+ * The implementation is called from CubicTest to execute an Custom Test Step.
+ * 
+ * @author SK Skytteren
+ */
 public interface ICustomTestStep {
 		
 	/**
 	 * Execute the test this element represents
+	 * 
+	 * @param arguments The properties that is set in the properties page in CubicTest.
+	 * @param context The purpose of the context is to send messages from one custom step to another instead of using static variables. 
+	 * @param selenium The Selenium Rich Client.
 	 */
 	public void execute(Map<String,String> arguments, IElementContext context, Selenium selenium);
 }
