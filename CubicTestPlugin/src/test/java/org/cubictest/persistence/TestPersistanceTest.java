81ec5ac6a2b4e2e27ce48f470562e17aa9a8bd71
skytteren <skytteren@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-05-28 20:50:30 +0000

    IN PROGRESS - issue CT-42: International characters corrupted into ??? when project is saved, tests can not be run. http://jira.openqa.org/browse/CT-42 - JUnit test
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1505 547eb39c-8521-0410-b6bb-ad8a2341d8b0

---
+++ CubicTestPlugin/src/test/java/org/cubictest/persistence/TestPersistanceTest.java
@@ -0,0 +1,40 @@
+package org.cubictest.persistence;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestPersistanceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetCharset() throws IOException {
+		File file = File.createTempFile("TempTest", "aat");
+		assertEquals("ISO-8859-1",TestPersistance.getCharset(file));
+	}
+	
+	@Test
+	public void testStoreTests() throws IOException{
+		org.cubictest.model.Test test = new org.cubictest.model.Test();
+		test.setName("NyTempTest");
+		assertNotNull(test);
+		File file = File.createTempFile("TempTest", "aat");
+		TestPersistance.saveToFile(test, file);
+		
+		org.cubictest.model.Test persistedTest = TestPersistance.loadFromFile(file, null);
+		assertEquals(test.toString(), persistedTest.toString() );
+	}
+
+}
