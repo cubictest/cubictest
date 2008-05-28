23d602cc9b1904911e3443ea2585c4fed82db266
skytteren <skytteren@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-05-28 21:31:38 +0000

    Adding more javadoc for exception
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1508 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestSeleniumExporterServer/src/main/java/org/cubictest/selenium/custom/ICustomTestStep.java
+++ CubicTestSeleniumExporterServer/src/main/java/org/cubictest/selenium/custom/ICustomTestStep.java
@@ -14,9 +14,14 @@ public interface ICustomTestStep {
 	/**
 	 * Execute the test this element represents
 	 * 
+	 * We expect you to user JUnit type assertions like assertEquals, etc. 
+	 * This way CubicTest can handle the CustomTestStep interaction correctly.
+	 * Please throw other kinds of exceptions as well.
+	 * 
 	 * @param arguments The properties that is set in the properties page in CubicTest.
 	 * @param context The purpose of the context is to send messages from one custom step to another instead of using static variables. 
 	 * @param selenium The Selenium Rich Client.
+	 * @throws any type of exception or error. It is handled by CubicTest.
 	 */
 	public void execute(Map<String,String> arguments, IElementContext context, Selenium selenium) throws Exception;
 }
