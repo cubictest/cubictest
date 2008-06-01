4a35f2487223c526d2b3996b303ef63031ebd1a8
skytteren <skytteren@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-06-01 21:35:45 +0000

    Adding shortcuts to launch...
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1539 547eb39c-8521-0410-b6bb-ad8a2341d8b0

---
+++ CubicTestSeleniumExporter/src/main/java/org/cubictest/exporters/selenium/launch/CubicTestSeleniumLaunchShortcut.java
@@ -0,0 +1,179 @@
+package org.cubictest.exporters.selenium.launch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.debug.ui.IDebugModelPresentation;
+import org.eclipse.debug.ui.ILaunchShortcut;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+
+public class CubicTestSeleniumLaunchShortcut implements ILaunchShortcut {
+
+	public void launch(ISelection selection, String mode) {
+		if(selection instanceof StructuredSelection) {
+			launch((IFile) ((StructuredSelection) selection).getFirstElement(), mode);
+		}
+	}
+
+	public void launch(IEditorPart editor, String mode) {
+		IEditorInput input = editor.getEditorInput();
+		IFile file = (IFile)input.getAdapter(IFile.class);
+		if(file != null){
+			launch(file, mode);
+		}
+	}
+
+	private void launch(IFile element, String mode) {
+		try {
+			ILaunchConfigurationWorkingCopy temparary = createLaunchConfiguration(element);
+			ILaunchConfiguration config = findExistingLaunchConfiguration(temparary, mode);
+			if (config == null) {
+				// no existing found: create a new one
+				config= temparary.doSave();
+			}
+			DebugUITools.launch(config, mode);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+	private ILaunchConfigurationWorkingCopy createLaunchConfiguration(IFile file) throws CoreException {
+		ILaunchConfigurationType configType = getLaunchManager().getLaunchConfigurationType(
+			"org.cubictest.exporters.selenium.ui.launchConfigurationType");
+		
+		ILaunchConfigurationWorkingCopy wc= configType.newInstance(file.getParent(), 
+				getLaunchManager().generateUniqueLaunchConfigurationNameFrom(file.getName()));
+		
+		wc.setAttribute(SeleniumRunnerTab.CUBIC_TEST_BROWSER, "*firefox");
+		wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, 
+				file.getProject().getName());
+		wc.setAttribute(SeleniumRunnerTab.CUBIC_TEST_NAME, 
+				file.getProjectRelativePath().toPortableString());
+		wc.setAttribute(SeleniumRunnerTab.CUBIC_TEST_NAMESPACE_XPATH, false);
+		wc.setAttribute(SeleniumRunnerTab.CUBIC_TEST_SELENIUM_SERVER_HOST, "localhost");
+		wc.setAttribute(SeleniumRunnerTab.CUBIC_TEST_SELENIUM_SERVER_PORT, "4545");
+		wc.setAttribute(SeleniumRunnerTab.CUBIC_TEST_SELENIUM_SERVER_MULTI_WINDOW, false);
+		LaunchConfigurationMigrationDelegate.mapResources(wc);
+		return wc;
+	}
+	
+	private ILaunchConfiguration findExistingLaunchConfiguration(
+			ILaunchConfigurationWorkingCopy temporary, String mode) throws CoreException, InterruptedException {
+		ILaunchConfigurationType configType= temporary.getType();
+
+		ILaunchConfiguration[] configs= getLaunchManager().getLaunchConfigurations(configType);
+		String[] attributeToCompare= getAttributeNamesToCompare();
+		
+		List<ILaunchConfiguration> candidateConfigs = 
+			new ArrayList<ILaunchConfiguration>(configs.length);
+		for (int i= 0; i < configs.length; i++) {
+			ILaunchConfiguration config= configs[i];
+			if (hasSameAttributes(config, temporary, attributeToCompare)) {
+				candidateConfigs.add(config);
+			}
+		}
+
+		// If there are no existing configs associated with the IType, create
+		// one.
+		// If there is exactly one config associated with the IType, return it.
+		// Otherwise, if there is more than one config associated with the
+		// IType, prompt the
+		// user to choose one.
+		int candidateCount= candidateConfigs.size();
+		if (candidateCount == 0) {
+			return null;
+		} else if (candidateCount == 1) {
+			return (ILaunchConfiguration) candidateConfigs.get(0);
+		} else {
+			// Prompt the user to choose a config. A null result means the user
+			// cancelled the dialog, in which case this method returns null,
+			// since cancelling the dialog should also cancel launching
+			// anything.
+			ILaunchConfiguration config = chooseConfiguration(candidateConfigs, mode);
+			if (config != null) {
+				return config;
+			}
+		}
+		return null;
+	}
+
+	private ILaunchManager getLaunchManager() {
+		return DebugPlugin.getDefault().getLaunchManager();
+	}
+	
+	protected String[] getAttributeNamesToCompare() {
+		return new String[] {
+				//SeleniumRunnerTab.CUBIC_TEST_BROWSER,
+				IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME,
+				SeleniumRunnerTab.CUBIC_TEST_NAME/*,
+				SeleniumRunnerTab.CUBIC_TEST_NAMESPACE_XPATH,
+				SeleniumRunnerTab.CUBIC_TEST_SELENIUM_SERVER_HOST,
+				SeleniumRunnerTab.CUBIC_TEST_SELENIUM_SERVER_PORT,
+				SeleniumRunnerTab.CUBIC_TEST_SELENIUM_SERVER_MULTI_WINDOW*/
+		};
+	}
+	
+	private static boolean hasSameAttributes(ILaunchConfiguration config1, ILaunchConfiguration config2, String[] attributeToCompares) {
+		try {
+			for (String attributeToCompare : attributeToCompares) {
+				if(SeleniumRunnerTab.CUBIC_TEST_NAMESPACE_XPATH.equals(attributeToCompare) ||
+						SeleniumRunnerTab.CUBIC_TEST_SELENIUM_SERVER_MULTI_WINDOW.equals(attributeToCompare)){
+					boolean val1 = config1.getAttribute(attributeToCompare, false);
+					boolean val2 = config2.getAttribute(attributeToCompare, false);
+					if (val1 != val2) {
+						return false;
+					}
+				}else{
+					String val1 = config1.getAttribute(attributeToCompare, "");
+					String val2 = config2.getAttribute(attributeToCompare, "");
+					if (!val1.equals(val2)) {
+						return false;
+					}
+				}
+			}
+			return true;
+		} catch (CoreException e) {
+			// ignore access problems here, return false
+		}
+		return false;
+	}
+	
+	private ILaunchConfiguration chooseConfiguration(List configList, String mode) throws InterruptedException {
+		IDebugModelPresentation labelProvider= DebugUITools.newDebugModelPresentation();
+		ElementListSelectionDialog dialog= new ElementListSelectionDialog(new Shell(), labelProvider);
+		dialog.setElements(configList.toArray());
+		dialog.setTitle("Select configuration");
+		if (mode.equals(ILaunchManager.DEBUG_MODE)) {
+			dialog.setMessage("Select debug configuration");
+		} else {
+			dialog.setMessage("Select run configuration");
+		}
+		dialog.setMultipleSelection(false);
+		int result= dialog.open();
+		if (result == Window.OK) {
+			return (ILaunchConfiguration) dialog.getFirstResult();
+		}
+		throw new InterruptedException(); // cancelled by user
+	}
+}
--- CubicTestSeleniumExporter/src/main/java/org/cubictest/exporters/selenium/launch/LaunchConfigurationMigrationDelegate.java
+++ CubicTestSeleniumExporter/src/main/java/org/cubictest/exporters/selenium/launch/LaunchConfigurationMigrationDelegate.java
@@ -10,22 +10,89 @@
  *******************************************************************************/
 package org.cubictest.exporters.selenium.launch;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationMigrationDelegate;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 
 public class LaunchConfigurationMigrationDelegate implements
 		ILaunchConfigurationMigrationDelegate {
 
-	public boolean isCandidate(ILaunchConfiguration candidate)
-			throws CoreException {
-		// TODO Auto-generated method stub
-		return false;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.debug.core.ILaunchConfigurationMigrationDelegate#isCandidate()
+	 */
+	public boolean isCandidate(ILaunchConfiguration candidate) throws CoreException {
+		IResource[] mapped = candidate.getMappedResources();
+		IResource target = getResource(candidate);
+		if (target == null) {
+			return mapped == null;
+		} else {
+			if (mapped == null) {
+				return true;
+			} else {
+				if (mapped.length != 1) {
+					return true;
+				} else {
+					return !target.equals(mapped[0]);
+				}
+			}
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.debug.core.ILaunchConfigurationMigrationDelegate#migrate(org.eclipse.debug.core.ILaunchConfiguration)
+	 */
 	public void migrate(ILaunchConfiguration candidate) throws CoreException {
-		// TODO Auto-generated method stub
-
+		ILaunchConfigurationWorkingCopy wc= candidate.getWorkingCopy();
+		mapResources(wc);
+		wc.doSave();
 	}
-
+	
+	/**
+	 * Maps a resource for the given launch configuration.
+	 * 
+	 * @param config working copy
+	 * @throws CoreException if an exception occurs mapping resource
+	 */
+	public static void mapResources(ILaunchConfigurationWorkingCopy config) throws CoreException {
+		IResource resource = getResource(config);
+		if (resource == null) {
+			config.setMappedResources(null);
+		} else {
+			config.setMappedResources(new IResource[]{resource});
+		}
+	}
+	
+	/**
+	 * Returns a resource mapping for the given launch configuration, or <code>null</code>
+	 * if none.
+	 * 
+	 * @param config working copy
+	 * @returns resource or <code>null</code>
+	 * @throws CoreException if an exception occurs mapping resource
+	 */
+	private static IResource getResource(ILaunchConfiguration config) throws CoreException {
+		String projName = config.getAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, (String)null);
+		String testName = config.getAttribute(SeleniumRunnerTab.CUBIC_TEST_NAME, (String)null);
+		if (projName != null && Path.ROOT.isValidSegment(projName)) {
+			IProject project = ResourcesPlugin.getWorkspace().
+				getRoot().getProject(projName);
+			if (project.exists()) {
+				if (testName != null) {
+					return (IResource)project.getFile(testName);
+				}
+			}
+		}
+		return null;
+	}
+	
 }
