f9ea783511b8850c64f706f8f4a17dd6e17e4281
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-07-08 20:29:06 +0000

    better label position
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1645 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestPlugin/src/main/java/org/cubictest/ui/gef/controller/TestEditPart.java
+++ CubicTestPlugin/src/main/java/org/cubictest/ui/gef/controller/TestEditPart.java
@@ -87,7 +87,7 @@ public class TestEditPart extends PropertyChangePart{
 			name += ": \n    " + ((Test)getModel()).getDescription();
 		}
 		Label nameLabel = new Label(name);
-		nameLabel.setLocation(new Point(170,15));
+		nameLabel.setLocation(new Point(170,25));
 		nameLabel.setTextAlignment(Label.TOP);
 		nameLabel.setLabelAlignment(Label.LEFT);
 		nameLabel.setSize(600, 50);
