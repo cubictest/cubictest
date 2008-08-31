5517f6e63f84748e1a50a78da3476d32f03c56d0
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-08-31 10:37:54 +0000

    now handles spaces in file names
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1690 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- copyToLocalRepo.sh
+++ copyToLocalRepo.sh
@@ -14,37 +14,37 @@ BASE_DIR=`pwd`
 # Copy hash CubicTest JARs to local repo:
 
 CUBICTEST_REPO_DIR=~/.m2/repository/org/cubictest/cubictest/$1
-if [ ! -d $CUBICTEST_REPO_DIR ] 
+if [ ! -d "$CUBICTEST_REPO_DIR" ] 
 then
-	mkdir $CUBICTEST_REPO_DIR
+	mkdir "$CUBICTEST_REPO_DIR"
 fi
 
 cd $BASE_DIR/CubicTestPlugin
-cp cubictest-$1.jar $CUBICTEST_REPO_DIR
-sha1 cubictest-$1.jar | awk '{print $1}' > $CUBICTEST_REPO_DIR/cubictest-$1.jar.sha1
-md5 cubictest-$1.jar | awk '{print $1}' > $CUBICTEST_REPO_DIR/cubictest-$1.jar.md5
+cp cubictest-$1.jar "$CUBICTEST_REPO_DIR"
+sha1 cubictest-$1.jar | awk '{print $1}' > "$CUBICTEST_REPO_DIR/cubictest-$1.jar.sha1"
+md5 cubictest-$1.jar | awk '{print $1}' > "$CUBICTEST_REPO_DIR/cubictest-$1.jar.md5"
 
-cp cubictest-$1.pom $CUBICTEST_REPO_DIR
-sha1 cubictest-$1.pom | awk '{print $1}' > $CUBICTEST_REPO_DIR/cubictest-$1.pom.sha1
-md5 cubictest-$1.pom | awk '{print $1}' > $CUBICTEST_REPO_DIR/cubictest-$1.pom.md5
+cp cubictest-$1.pom "$CUBICTEST_REPO_DIR"
+sha1 cubictest-$1.pom | awk '{print $1}' > "$CUBICTEST_REPO_DIR/cubictest-$1.pom.sha1"
+md5 cubictest-$1.pom | awk '{print $1}' > "$CUBICTEST_REPO_DIR/cubictest-$1.pom.md5"
 
 
 # Copy and hash Selenium exporter JARs to local repo:
 
 SELENIUM_EXPORTER_DIR=~/.m2/repository/org/cubictest/selenium-exporter/$1
-if [ ! -d $SELENIUM_EXPORTER_DIR ]
+if [ ! -d "$SELENIUM_EXPORTER_DIR" ]
 then
-	mkdir $SELENIUM_EXPORTER_DIR
+	mkdir "$SELENIUM_EXPORTER_DIR"
 fi
 
 cd $BASE_DIR/CubicTestSeleniumExporter
-cp selenium-exporter-$1.jar $SELENIUM_EXPORTER_DIR
-sha1 selenium-exporter-$1.jar | awk '{print $1}' > $SELENIUM_EXPORTER_DIR/selenium-exporter-$1.jar.sha1
-md5 selenium-exporter-$1.jar | awk '{print $1}' > $SELENIUM_EXPORTER_DIR/selenium-exporter-$1.jar.md5
+cp selenium-exporter-$1.jar "$SELENIUM_EXPORTER_DIR"
+sha1 selenium-exporter-$1.jar | awk '{print $1}' > "$SELENIUM_EXPORTER_DIR/selenium-exporter-$1.jar.sha1"
+md5 selenium-exporter-$1.jar | awk '{print $1}' > "$SELENIUM_EXPORTER_DIR/selenium-exporter-$1.jar.md5"
 
 cp selenium-exporter-$1.pom $SELENIUM_EXPORTER_DIR
-sha1 selenium-exporter-$1.pom | awk '{print $1}' > $SELENIUM_EXPORTER_DIR/selenium-exporter-$1.pom.sha1
-md5 selenium-exporter-$1.pom | awk '{print $1}' > $SELENIUM_EXPORTER_DIR/selenium-exporter-$1.pom.md5
+sha1 selenium-exporter-$1.pom | awk '{print $1}' > "$SELENIUM_EXPORTER_DIR/selenium-exporter-$1.pom.sha1"
+md5 selenium-exporter-$1.pom | awk '{print $1}' > "$SELENIUM_EXPORTER_DIR/selenium-exporter-$1.pom.md5"
 
 
 # Maven 2 runner:
