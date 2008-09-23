cdfdf5fc5c979bdf819be1af0586f72c0e2d1911
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-09-23 11:20:55 +0000

    updated
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1744 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- copyToBoss.sh
+++ copyToBoss.sh
@@ -1,12 +1,12 @@
 echo "Repo files:"
 scp -r ./cubictest_m2_jars/* schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/m2repo/
 
-echo "Repo zip file:"
-scp -r ./cubictest_m2_jars.zip schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/m2repo/
+#echo "Repo zip file:"
+#scp -r ./cubictest_m2_jars.zip schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/m2repo/
 
-echo "Update site files:"
-scp -r ./CubicTestLocalUpdateSite/*  schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/update/
+#echo "Update site files: (CTRL+C to abort)"
+#scp -r ./CubicTestLocalUpdateSite/*  schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/update/
 
-echo "Update site zip file:"
-scp -r ./CubicTestLocalUpdateSite.zip  schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/update/
+#echo "Update site zip file:"
+#scp -r ./CubicTestLocalUpdateSite.zip  schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/update/
 
