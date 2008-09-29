3d52741216b89c917c682020d2d2266f8da76a6b
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-09-29 18:36:31 +0000

    now zips update site
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1761 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- copyToBossRC.sh
+++ copyToBossRC.sh
@@ -2,13 +2,14 @@
 
 if [ ! $1 ]
 then
-        echo "Please provide a version number, e.g. 1.8.5-rc1"
+        echo "Please provide a version number, e.g. 1.8.5"
         exit -1
 fi
 
+./zipUpdateSite.sh $@
+echo "OK to continue? (CTRL+C to abort)"
+read
 
-echo "Repo files: (CTRL+C to skip)"
-scp -r ./cubictest_m2_jars/* schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/m2repo/
 
 #echo "Repo zip file:"
 #scp -r ./cubictest_m2_jars.zip schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/m2repo/
