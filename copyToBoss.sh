63d2b5a9fb570177ccc03811d3517b0c3876c602
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2008-09-29 16:27:52 +0000

    better..
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1758 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- copyToBoss.sh
+++ copyToBoss.sh
@@ -1,3 +1,17 @@
+if [ ! $1 ]
+then
+        echo "Please provide a version number, e.g. 1.8.5"
+        exit -1
+fi
+
+./copyToLocalRepo.sh $@
+echo "OK to continue?"
+read
+
+./zipRepo.sh $@
+echo "OK to continue?"
+read
+
 echo "Repo files:"
 scp -r ./cubictest_m2_jars/* schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/m2repo/
 
