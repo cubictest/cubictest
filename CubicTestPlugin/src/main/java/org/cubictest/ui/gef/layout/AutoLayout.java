3bc49a31c57ea89cae896883efb6e4cafb9094bb
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2009-01-24 22:38:48 +0000

    better autolayout
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1857 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestPlugin/src/main/java/org/cubictest/ui/gef/layout/AutoLayout.java
+++ CubicTestPlugin/src/main/java/org/cubictest/ui/gef/layout/AutoLayout.java
@@ -172,9 +172,10 @@ public class AutoLayout {
 			numOfTrans = numOfTrans < 1 ? 1 : numOfTrans;
 			int newX = topCenter.x + (numOfTrans - 1) * ITestEditor.NEW_PATH_OFFSET;
 			int newY = bottom + TRANSITION_SPACE + (numOfActions * USER_INPUT_HEIGHT);
-			if (t.getEnd() instanceof ExtensionPoint && node.getOutTransitions().size() > 1) {
+			if (t.getEnd() instanceof ExtensionPoint) {
 				newY = node.getPosition().y + node.getDefaultDimension().height / 2;
-				newX = node.getPosition().x + node.getDimension().width + 120;
+				newX = node.getPosition().x + node.getDimension().width + 150;
+				numOfTrans--; //since exPoint is placed at side
 			}
 			layout(t.getEnd(), new Point(newX, newY));
 		}
