8d3b2f11e108101fda2d73f666507db555ad5ac2
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2009-02-10 07:02:39 +0000

    init
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1899 547eb39c-8521-0410-b6bb-ad8a2341d8b0

---
+++ copyToBossReleaseUpdateSite.sh
@@ -0,0 +1,14 @@
+if [ ! $1 ]
+then
+        echo "Please provide a version number, e.g. 1.8.5"
+        exit -1
+fi
+
+echo "Update site files: (CTRL+C to abort)"
+scp -r ./CubicTestLocalUpdateSite/*  schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/update/
+
+./zipUpdateSite.sh
+
+echo "Update site zip file:"
+scp -r ./CubicTestLocalUpdateSite.zip  schwarz@boss.bekk.no:/srv/www/htdocs/cubictest/update/
+
