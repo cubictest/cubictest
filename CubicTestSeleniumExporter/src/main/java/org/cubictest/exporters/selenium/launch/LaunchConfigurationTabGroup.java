a57fcf514491779219727f2c133e878c88d1c2f7
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2009-08-16 17:50:17 +0000

    comment
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@1976 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestSeleniumExporter/src/main/java/org/cubictest/exporters/selenium/launch/LaunchConfigurationTabGroup.java
+++ CubicTestSeleniumExporter/src/main/java/org/cubictest/exporters/selenium/launch/LaunchConfigurationTabGroup.java
@@ -17,13 +17,11 @@ import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
-import org.eclipse.jdt.debug.ui.launchConfigurations.JavaConnectTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab;
 
 public class LaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup{
 
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
-		// TODO Auto-generated method stub
 		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
 				new SeleniumRunnerTab(),
 				new JavaArgumentsTab(),
