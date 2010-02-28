6074c8b88ec1be91facb9dad0b47a92d5c53806e
chr_schwarz <chr_schwarz@547eb39c-8521-0410-b6bb-ad8a2341d8b0>
2010-02-28 10:18:11 +0000

    updated date in license (and added license to files that did not have it)
    git-svn-id: http://svn.openqa.org/svn/cubictest/trunk@2077 547eb39c-8521-0410-b6bb-ad8a2341d8b0

--- CubicTestSeleniumExporter/src/main/java/org/cubictest/exporters/selenium/common/BrowserType.java
+++ CubicTestSeleniumExporter/src/main/java/org/cubictest/exporters/selenium/common/BrowserType.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 Christian Schwarz and Stein K. Skytteren
+ * Copyright (c) 2005, 2010 Stein K. Skytteren and Christian Schwarz
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *    Christian Schwarz and Stein K. Skytteren - initial API and implementation
+ *    Stein K. Skytteren and Christian Schwarz - initial API and implementation
  *******************************************************************************/
 package org.cubictest.exporters.selenium.common;
 
